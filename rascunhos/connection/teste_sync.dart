627d363395175705b2d8c46eb0f15cf36af9f317
NoobLogado <noobtb@outlook.com>
2021-10-26 01:13:00 -0300

    noobzin

---
+++ rascunhos/connection/teste_sync.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: avoid_print
+//teste mod config commit
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:ifeira_testes/pages/setUrl_page.dart';
+
+import '../objetos/usuario.dart';
+
+/*void main() {
+  fetch();
+}*/
+
+Future fetch() async {
+  var url = Uri.parse('setUrl'); //puxar a 'setUrl' pra ca
+  Map<String, String> teste = {
+    "nome": "Caio Cesar",
+    "email": "teste@teste",
+    "senha": "12321"
+  };
+  Map<String, String> header = { 'Content-type': 'application/json',
+    'Accept': 'application/json',
+    "Authorization": "Some token"};
+  var client = http.Client();
+  var response = await client.post(url,body: jsonEncode(teste), headers: {'Content-type': 'application/json'});
+  print(response.statusCode);
+  if (response.statusCode == 200) {
+    // If the server returns an OK response, then parse the JSON.
+    return print(Welcome.fromJson(json.decode(response.body)));
+  }
+  }
